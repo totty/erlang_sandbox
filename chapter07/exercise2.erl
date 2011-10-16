3e35f10154f7358a233a41e34930d2667e942954
Takahisa Tokunaga <tokunaga.jp@gmail.com>
2011-10-17 00:46:11 +0900

    exercise7-2 問題の意味を勘違いしていたようなので修正

--- chapter07/exercise2.erl
+++ chapter07/exercise2.erl
@@ -5,9 +5,7 @@
 
 joe() -> #person{name="Joe", age=21, phone="999-999"}.
 
-%% foobar(#person{name=Name, age=Age} = P) when Record(P, person) ->
-%%     io:format("name: ~p, age: ~p, phone: ~p~n", [Name, Age, Phone]);
-foobar(P) when is_record(P, person) ->
-    io:format("record for: ~p~n", [P]);
+foobar(P) when is_record(P, person), P#person.name == "Joe" ->
+    io:format("joe record for: ~p~n", [P]);
 foobar(P) ->
-    io:format("not record: ~p~n", [P]).
+    io:format("not joe record: ~p~n", [P]).
